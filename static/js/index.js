70f1f1e16b9aa8db8a8b1320b03034990b6de253
jwnpoh <jwn.poh@gmail.com>
2021-08-27 00:18:27 +0800

    changed and not working

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,40 @@
-let btn = document.getElementById("submit");
-btn.addEventListener("click", () => {
+window.addEventListener('DOMContentLoaded', () => {
+  let btn = document.getElementById("submit");
+  btn.addEventListener("click", loadingMessage);
+
+  let uploadFile = document.getElementById("upload");
+  uploadFile.addEventListener("change", renderPreview);
+});
+
+function loadingMessage() {
   let txt = btn.innerText;
   txt = 'Please wait while your image is being processed...\n(this might take a while, depending on the resolution of your image)';
   btn.setAttribute("aria-busy", "true");
   btn.innerText = txt;
-});
+}
 
-let uploadFile = document.getElementById("upload");
-uploadFile.addEventListener("change", () => {
+function renderPreview() {
   let file = uploadFile.files;
   if (file.length > 0) {
-    const fileType = file[0].type;
+    let imageFile = file[0];
+    checkFileType(imageFile);
+    checkFileSize(imageFile);
+    renderImg(imageFilee);
+  } 
+}
+
+function checkFileType(file) {
+    const fileType = file.type;
     if (fileType != "image/jpeg" && fileType != "image/png") {
       notie.alert({type: 'error', text:'The selected file does not seem to be an image file. Please try another file.'})
       return btn.disabled = true;
       } else {
           btn.disabled = false;
     }
-    const fileSize = file[0].size;
+}
+
+function checkFileSize(file) {
+    const fileSize = file.size;
     const fileSizeMB = Math.round((fileSize/1024/1024));
       if (fileSizeMB > 4) {
       notie.alert({type: 'error', text:'The selected file exceeds 4mb in size. Please select a smaller file.'})
@@ -25,12 +42,13 @@ uploadFile.addEventListener("change", () => {
       } else {
           btn.disabled = false;
       }
+}
 
-    let imageFile = file[0];
+function renderImg(file) {
     var reader = new FileReader();
-        reader.onload = (e) => {
+        reader.onload = () => {
         var img = document.createElement("img");
-            img.onload = (event) => {
+            img.onload = () => {
               var MAX_WIDTH = 640;
               var width = img.width;
               var height = img.height;
@@ -52,12 +70,10 @@ uploadFile.addEventListener("change", () => {
               ctx.drawImage(img, 0, 0, width, height);
 
               // Show resized image in preview element
-              var dataurl = canvas.toDataURL(imageFile.type);
+              var dataurl = canvas.toDataURL(file.type);
               document.getElementById("preview").src = dataurl;
             }
         img.src = e.target.result;
       }
-    reader.readAsDataURL(imageFile);
-  } 
-});
-
+    reader.readAsDataURL(file);
+}
